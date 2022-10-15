386e3bb961652fb24257e63a5938f737d9b69a28
nllosas <nllosas@fi.uba.ar>
2022-10-15 03:06:54 -0300

    landing page done?

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import React from 'react';
 import { Route } from "react-router-dom";
 import LandingPage from './components/LandingPage/LandingPage';
--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -4,11 +4,23 @@ import style from './LandingPage.module.css';
 
 function LandingPage() {
   return (
-    <div className={style.container}>
-        <h1>Bienvenido a la pagina de Dogs</h1>
-        <Link to='/home'>
-            <button>Ingresar</button>
-        </Link>
+    <div className={style.background}>
+      <div className={style.main_container}>
+        <div className={style.inner_text_container}>
+          <div className={style.title}>
+            <h1>We Love Dogs!</h1>
+          </div>
+          <Link to='/home' className={style.enter_button}>
+            Enter
+          </Link>
+        </div>
+        <div className={style.inner_images_container}>
+          <div className={style.big_image}/>
+          <div className={style.small_image_top_left}/>
+          <div className={style.small_image_middle_right}/>
+          <div className={style.small_image_bottom_left}/>
+        </div>
+      </div>
     </div>
   )
 }
