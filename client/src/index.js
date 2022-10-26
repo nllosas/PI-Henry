de1a6aa5a2fa25a4e6fef27234dac87348a0ba1d
nllosas <nllosas@fi.uba.ar>
2022-10-25 22:48:05 -0300

    d

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import { Provider } from 'react-redux';
 import store from "./store";
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
+axios.defaults.baseURL = process.env.REACT_APP_API || 'https://henry-deploy-pi.herokuapp.com';
 
 ReactDOM.render(
   <Provider store={store}>
