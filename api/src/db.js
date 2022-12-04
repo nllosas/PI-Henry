9e38b0e7383329caab53a0c589cfdcac94b53094
nllosas <nllosas@fi.uba.ar>
2022-12-04 14:04:44 -0300

    updated for deploy

--- api/src/db.js
+++ api/src/db.js
@@ -44,7 +44,7 @@ const sequelize = new Sequelize(
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-    dialectModule: pg,
+    // dialectModule: pg,
   }
 );
 
